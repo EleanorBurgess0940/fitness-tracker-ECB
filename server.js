7dedc049e0d24039218abc087e75fb7730776b91
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-06-16 21:13:40 -0400

    added functionality to the html routes

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
 // routes
-// app.use(require("./routes/api.js"));
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
