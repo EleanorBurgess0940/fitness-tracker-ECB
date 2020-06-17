ff1b73d22981b33cd2cc4e38dddf7a730c67cf6a
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-06-17 16:29:04 -0400

    added workout.js to the models folder

---
+++ models/workout.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercise: [
+    {
+      type: {
+        type: String,
+        required: "Exercise Type is required",
+      },
+      name: {
+        type: String,
+        required: "Exercise Name is required",
+      },
+      weight: {
+        type: Number,
+        required: true,
+      },
+      sets: {
+        type: Number,
+        required: true,
+      },
+      reps: {
+        type: Number,
+        required: true,
+      },
+      resistanceDuration: {
+        type: Number,
+        required: true,
+      },
+      cardioName: {
+        type: String,
+        required: "Exercise Name is required",
+      },
+      distance: {
+        type: Number,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
