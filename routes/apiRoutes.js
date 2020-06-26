095b0b11dca8191b16ca520816c1ae8c73a410ca
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-06-25 20:51:31 -0400

    changed a put route to a get route and now the statistics work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,6 @@ const Workout = require("../models/workouts.js");
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .then((dbWorkout) => {
-      console.log(dbWorkout);
       res.json(dbWorkout);
     })
     .catch((err) => {
@@ -16,14 +15,13 @@ router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
     .then((dbWorkout) => {
       res.json(dbWorkout);
-      console.log("Postin shit");
     })
     .catch((err) => {
       res.status(400).json(err);
     });
 });
 
-router.post("/api/workouts/range", (req, res) => {
+router.get("/api/workouts/range", (req, res) => {
   Workout.find()
     .then((data) => {
       res.json(data);
