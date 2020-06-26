60b0dc9626cdb89a0ef0caba518973cd1d286bba
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-06-25 20:12:22 -0400

    updated routes to get the latest workout to be seen

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,6 +4,7 @@ const Workout = require("../models/workouts.js");
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .then((dbWorkout) => {
+      console.log(dbWorkout);
       res.json(dbWorkout);
     })
     .catch((err) => {
@@ -15,6 +16,17 @@ router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
     .then((dbWorkout) => {
       res.json(dbWorkout);
+      console.log("Postin shit");
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts/range", (req, res) => {
+  Workout.find()
+    .then((data) => {
+      res.json(data);
     })
     .catch((err) => {
       res.status(400).json(err);
@@ -22,7 +34,7 @@ router.post("/api/workouts", ({ body }, res) => {
 });
 
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-  Workout.findByIdAndUpdate({ _id: params.id }, { $push: { exercise: body } })
+  Workout.findByIdAndUpdate({ _id: params.id }, { $push: { exercises: body } })
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
